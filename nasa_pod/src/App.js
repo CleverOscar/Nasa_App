1c2ebacccd6480d29921e1b59e36ae118326ea83
Oscar Pacheco <Oscarpa9188@gmail.com>
2021-12-19 16:47:33 -0600

    API request
    Need to hide API key to make request safely

--- nasa_pod/src/App.js
+++ nasa_pod/src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  // init empty array for Nasa Data
+  const [nasaData, setNasaData] = useState([]);
+
+  // fetch nasa api 
+  const nasaApi = useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?", 
+    { 
+      headers: 
+      {  
+        "Authorization": process.env.REACT_APP_NASA_API 
+      } 
+    })
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((error) => {
+      console.log(error.message)
+    })
+  
+  }, []);
+
+  console.log("Api: ", process.env.REACT_APP_NASA_API)
+
   return (
-    <div className="container">
-      <header className>
+    <div className="container mx-auto">
+      <header >
         <h1>Nasa Photo Of The Day</h1>
       </header>
     </div>
