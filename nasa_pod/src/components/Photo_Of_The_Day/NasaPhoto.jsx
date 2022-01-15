20459657c2299e2c70ab57231889f4ef0952ab26
Oscar Pacheco Ortiz <Oscarpa9188@gmail.com>
2022-01-14 23:05:54 -0600

    Update NasaPhoto.jsx

--- nasa_pod/src/components/Photo_Of_The_Day/NasaPhoto.jsx
+++ nasa_pod/src/components/Photo_Of_The_Day/NasaPhoto.jsx
@@ -18,21 +18,24 @@ export default function NasaPhoto(props){
     
     }, []);
 
-    return(
-        <div className="mx-6">
-            <h1 className="text-center text-2xl my-4 underline">
-                Astronomy Photo Of The Day
-            </h1>
+    return(<div className="mx-6">
+        <h1 className="text-center text-2xl my-4 underline">
+          Astronomy Photo Of The Day
+        </h1>
 
-            <img className="border-4 border-black rounded-xl md:mx-auto md:w-8/12 " src={nasaData.url} alt={nasaData.media_type} />
-
-            <div className="p-4">
-              <p className='text-lg  italic text-cyan-500 md:text-center md:text-2xl lg:text-4xl'> 
-                Name: {nasaData.title}
+        <div className='lg:grid lg:grid-cols-2 lg:items-center'>
+            <div>
+              <p className='text-lg  italic text-cyan-500 md:text-center md:text-2xl lg:text-3xl py-8'> 
+                Name : {nasaData.title}
               </p>
+              <img className="shadow-2xl shadow-blue-700/100 border-4 border-black rounded-xl md:mx-auto md:w-8/12 " src={nasaData.url} alt={nasaData.media_type} />
+            </div>
+            <div className="p-4 ">
               <p className='mb-4 text-gray-500 md:text-center md:text-xl lg:text-2xl'>Taken By: {nasaData.copyright}</p>
-              <p className='text-lg border-double border-8 border-black p-6 rounded-lg shadow-2xl'>{nasaData.explanation}</p>
+              <p className='text-lg border-double border-8 border-black p-6 rounded-lg shadow-2xl shadow-blue-700/100'>{nasaData.explanation}</p>
             </div>
         </div>
+
+    </div>
     )
 }
