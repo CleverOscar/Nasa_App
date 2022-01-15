eac1dacd59f17f3c5c59b26b176e0dd487a35ca1
Oscar Pacheco <Oscarpa9188@gmail.com>
2022-01-15 12:41:14 -0600

    Update Navbar.jsx
    Mobile Navbar in the works

--- nasa_pod/src/components/Navbar.jsx
+++ nasa_pod/src/components/Navbar.jsx
@@ -1,23 +1,52 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+const midScreenNav = <div className='hidden md:block border-2 border-blue-500 py-4'>
+                        <ul className='text-base flex flex-row items-center text-center'>
+                            <Link className="w-1/4"  to="/">
+                                Home
+                            </Link>
+                            <Link className="w-1/4" to='/apod'>
+                                Photo Of The Day
+                            </Link>
+                            <Link className="w-1/4" to='/mars'>
+                                Mars Rover Photos
+                            </Link>
+                            <Link className="w-1/4" to="/earth">
+                                Earth
+                            </Link>
+                        </ul>
+                    </div>
+
+const smallScreenNav = <div className='flex flex-row block border-2 border-red-500 p-4  md:hidden'>
+                            <p className="w-full">Nasa Portal</p>
+                            <ul className='text-base  items-center text-center border '>
+                                Nav
+                            </ul>
+                        </div>
+
+
 export default function Navbar() {
     return(
-        <div className='border-2 py-4'>
-            <ul className='text-base flex flex-row items-center text-center'>
-                <Link className="w-1/4"  to="/">
-                    Home
-                </Link>
-                <Link className="w-1/4" to='/apod'>
-                    Photo Of The Day
-                </Link>
-                <Link className="w-1/4" to='/mars'>
-                    Mars Rover Photos
-                </Link>
-                <Link className="w-1/4" to="/earth">
-                    Earth
-                </Link>
-            </ul>
+        <div>
+            {midScreenNav}
+            {smallScreenNav}
         </div>
     )
 }
+
+
+{/* <ul className='text-base flex flex-col items-center text-center'>
+<Link className="w-1/4"  to="/">
+Home
+</Link>
+<Link className="w-1/4" to='/apod'>
+Photo Of The Day
+</Link>
+<Link className="w-1/4" to='/mars'>
+Mars Rover Photos
+</Link>
+<Link className="w-1/4" to="/earth">
+Earth
+</Link>
+</ul> */}
