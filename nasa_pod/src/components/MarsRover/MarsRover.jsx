ab24e2c7dd129e6c56058621c0b1c1a63ad40157
Oscar Pacheco Ortiz <Oscarpa9188@gmail.com>
2022-01-14 21:31:06 -0600

    Update MarsRover.jsx
    User is now allowed to change date and get updated photos on that specific date.

--- nasa_pod/src/components/MarsRover/MarsRover.jsx
+++ nasa_pod/src/components/MarsRover/MarsRover.jsx
@@ -56,13 +56,12 @@ export default function MarsRover(){
     const api = "JbPskfAcVPpxN602YevCVKqXG7dh7VZ7Yb8qkM2j"
 
     // Date
-    let yourDate = new Date().toISOString().split('T')[0];
+    let [yourDate, setYourDate] = useState(new Date().toISOString().split('T')[0]);
 
     // url 
     let  url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${yourDate}&page=${pageNumebr}&api_key=${api}`;
 
     // https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2020-6-4&page=${pageNumebr}&camera=${cameraState}&api_key=${api}
-    console.log(yourDate)
 
 
     console.log(url)
@@ -75,48 +74,65 @@ export default function MarsRover(){
            }
        )
     },[])
-    
-    function handleSubmit(event){
-        event.preventDefault();
-        return console.log("Submitted")
-    }
-            
-//     axios.get(url)
-//     .then((response) => {setRoverImages(response.data.photos)})
-//     .catch((error) => {console.log(error)})
-//      console.log(roverImages);
-    
-    function getValue(event){
-        setCameraState(event.target.value)
-        return(console.log(cameraState));
-    }
 
-       const images = roverImages.map(item => 
-            <div className="border-4 mt-10" key={item.id}>
-                <p>Rover: {item.rover.name}</p>
-                <p>Camera Name: {item.camera.full_name}</p>
-                <p>Solar Day: {item.sol}</p>
-                <p>Earth Day: {item.earth_date}</p>
-                <img alt={item.name} src={item.img_src} />
-            </div>
-        )
+    const images = roverImages.map(item => 
+        <div className="border-4 mt-10" key={item.id}>
+            <p>Rover: {item.rover.name}</p>
+            <p>Camera Name: {item.camera.full_name}</p>
+            <p>Solar Day: {item.sol}</p>
+            <p>Earth Day: {item.earth_date}</p>
+            <img alt={item.name} src={item.img_src} />
+        </div>
+    )
 
     const cameraOption = cameras.map(camera => 
         <option key={camera.id} defaultValue={camera.camera_name}>
             {camera.camera_name}
         </option>)
 
+    
+    const updateDate = <div>
+        <label className='px-2'>
+        Date: 
+        </label>
+        <input id='myDate' type="date" onChange={getDate}/>
+    </div>
+    
+    function getDate(){
+        return setYourDate(document.getElementById("myDate").value);
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        
+        axios.get(url).then(
+            (res)=>{
+             setRoverImages(res.data.photos)
+            }
+        )
+
+        return console.log("Submitted")
+    }
+    
+    function getValue(event){
+        setCameraState(event.target.value)
+        return(console.log(cameraState));
+    }
+
 
     return(
         <div className=" mt-8 w-10/12 mx-auto">
             <form className="w-auto flex flex-row my-6" onSubmit={handleSubmit}>
                 <div className="mx-auto">
+                   <div>
                     <label className="border-2 px-10 mr-6">
-                        Camera Viewing
-                    </label>
-                    <select className="border w-auto text-center text-red-800" id="cameras" onChange={getValue}>
-                       {cameraOption}
-                    </select>
+                            Camera Viewing
+                        </label>
+                        <select className="border w-auto text-center text-red-800" id="cameras" onChange={getValue}>
+                        {cameraOption}
+                        </select>
+                   </div>
+                    {updateDate}
                 </div>
                 <button className="mx-auto border-2 border-blue-500 px-4" type="sumbit">Search</button>
             </form>
