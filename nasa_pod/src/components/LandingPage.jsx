b1d723be9927a54e6f475b17edb41b16492fd15c
Oscar Pacheco <Oscarpa9188@gmail.com>
2022-01-15 12:07:32 -0600

    Update LandingPage.jsx
    Added Nasa Logo to landing page

--- nasa_pod/src/components/LandingPage.jsx
+++ nasa_pod/src/components/LandingPage.jsx
@@ -24,6 +24,8 @@ export default function LandingPage(){
             <p className='px-6 pb-6 text-2xl'>
             A React App that fetches data from NASA's API portal. Displaying photos and videos from Earth, Mars, Stars, Galaxies, and more!
             </p>
+
+            <img src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png" alt="nasa logo" />
         </div>
     )
 }
