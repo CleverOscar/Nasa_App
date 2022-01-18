c978afb5248c99f6f695effbce51650047f0edb6
Oscar Pacheco Ortiz <Oscarpa9188@gmail.com>
2022-01-18 01:11:42 -0600

    Update NasaPhoto.jsx
    Date picker functionality working for Photo of the day page

--- nasa_pod/src/components/Photo_Of_The_Day/NasaPhoto.jsx
+++ nasa_pod/src/components/Photo_Of_The_Day/NasaPhoto.jsx
@@ -2,13 +2,20 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 export default function NasaPhoto(props){
+  
+  // init empty array for Nasa Data
+  let [nasaData, setNasaData] = useState([]);
 
-      // init empty array for Nasa Data
-  const [nasaData, setNasaData] = useState([]);
+  // date
+  let [dateSetup, setDateSetup] = useState(new Date().toISOString().split('T')[0]) 
+
+  const api = 'JbPskfAcVPpxN602YevCVKqXG7dh7VZ7Yb8qkM2j';
+
+  let url  = `https://api.nasa.gov/planetary/apod?date=${dateSetup}&api_key=${api}`;
 
       // fetch nasa api 
     useEffect(() => {
-      axios.get("https://api.nasa.gov/planetary/apod?api_key=JbPskfAcVPpxN602YevCVKqXG7dh7VZ7Yb8qkM2j")
+      axios.get(url)
       .then((res) => {
         setNasaData(res.data)
       })
@@ -17,14 +24,34 @@ export default function NasaPhoto(props){
       })
     
     }, []);
+    
 
-    const date = <div className='border-2 border-blue-800 px-4 py-2 rounded-lg'>
-        <label > Pick a date to checkout a certain date: </label>
-        <input className='border-2 border-black rounded px-4 w-auto' id="myDate" type="date" />
-    </div>
+  const date = <div className='border-2 border-blue-800 px-4 py-2 rounded-lg flex flex-col w-10/12 mx-auto lg:w-1/2'>
+      <label  className='text-xl py-2 text-center'> Pick a date to checkout a certain date: </label>
+      <input className='border-2 border-black rounded px-4 w-10/12 mx-auto text-xl' id="myDate" type="date" max={new Date().toISOString().split('T')[0]} onChange={getDate}/>
+      <button className="mx-auto border-2 border-blue-500 px-4" type="sumbit">Search</button>
+  </div>
+
+  function getDate(){
+    return setDateSetup(document.getElementById("myDate").value)
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+
+    console.log(dateSetup)
+    console.log(url)
+    axios.get(url).then(
+      (res) => {
+        setNasaData(res.data)
+      }
+    )
+  }
 
     return(<div className="mx-6">
-        {date}
+        <form onSubmit={handleSubmit}>
+          {date}
+        </form>
         <p className="text-center text-2xl my-6 underline md:shadow-none">
           Astronomy Photo Of The Day
         </p>
@@ -34,7 +61,7 @@ export default function NasaPhoto(props){
               <p className='hidden lg:block text-2xl py-8 text-center italic text-cyan-500 '> 
                 Name : {nasaData.title}
               </p>
-              <img className="shadow-2xl  border-4 border-black rounded-xl lg:w-full" src={nasaData.url} alt={nasaData.media_type} />
+              <img className="shadow-2xl mx-auto  border-4 border-black rounded-xl lg:w-full" src={nasaData.url} alt={nasaData.media_type} />
               <p className='text-xl text-center italic text-cyan-500 md:text-center md:text-2xl py-8 lg:hidden'> 
                 Name : {nasaData.title}
               </p>
