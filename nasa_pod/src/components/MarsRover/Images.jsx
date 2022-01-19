4db43474e077f17b82bf0e320d25322c3ff3e07b
Oscar Pacheco <Oscarpa9188@gmail.com>
2022-01-18 19:13:56 -0600

    Update Images.jsx
    Rover Images Styles Updated

--- nasa_pod/src/components/MarsRover/Images.jsx
+++ nasa_pod/src/components/MarsRover/Images.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 export default function Images(props) {
 
     const images = props.data.map(item => 
-        <div className="border-4 mt-10" key={item.id}>
-            <p>Rover: {item.rover.name}</p>
-            <p>Camera Name: {item.camera.full_name}</p>
-            <p>Solar Day: {item.sol}</p>
-            <p>Earth Day: {item.earth_date}</p>
-            <img alt={item.name} src={item.img_src} />
+        <div className="border-4 border-black p-6 mt-10 shadow-2xl rounded-lg lg:w-auto lg:mx-auto lg:flex lg:flex-row  lg:items-center" key={item.id}>
+            <img className="mx-auto w-10/12 border-black border-4 rounded-lg lg:w-1/2" alt={item.name} src={item.img_src} />
+            <div className="text-2xl w-10/12 mx-auto border-4 border-black bg-gray-200 rounded-lg px-6 py-2 mt-4 uppercase mx-auto lg:mx-8 lg:px-6 lg:w-8/12 lg:text-3xl lg:text-left lg:mt-none">
+                <p className="underline my-2">Rover: {item.rover.name}</p>
+                <p className="text-red-500">Camera View: {item.camera.full_name}</p>
+                <p className="text-green-500">Solar Day: {item.sol}</p>
+                <p className="text-blue-500">Earth Day: {item.earth_date}</p>
+                <p className="text-green-800">Status: {item.rover.status}</p>
+            </div>
         </div>
     )
     
