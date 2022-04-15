65313df30c1e9277528fbf607630a723fae1939a
Oscar Pacheco <Oscarpa9188@gmail.com>
2022-04-15 14:47:54 -0500

    Fetch Data
    Photo Of The Day Component
    
    The fetch data button now retrieves the data we requested

--- nasa_pod/src/components/Photo_Of_The_Day/NasaPhoto.jsx
+++ nasa_pod/src/components/Photo_Of_The_Day/NasaPhoto.jsx
@@ -5,21 +5,7 @@ import OptionsTab from './views/OptionsTab';
 
 
 export default function NasaPhoto(){
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-  var yyyy = today.getFullYear();
-  var todaysDate = yyyy + '-' + mm + '-' + dd;
 
-  // api key
-  const api = 'JbPskfAcVPpxN602YevCVKqXG7dh7VZ7Yb8qkM2j';
-
-  // init empty array for Nasa Data
-  let [nasaData, setNasaData] = useState([]);
-  let [date, setDate] = useState(todaysDate);
-
-  // nasa api url
-  let url = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${api}`
 
 
     return(<div className="mt-6 w-11/12 mx-auto flex flex-col" >
--- nasa_pod/src/components/Photo_Of_The_Day/views/PhotoOfTheDay.jsx
+++ nasa_pod/src/components/Photo_Of_The_Day/views/PhotoOfTheDay.jsx
@@ -1,7 +1,23 @@
 import React, {useState} from 'react';
+import axios from 'axios'
 
 function PhotoOfTheDay(){
 
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    var todaysDate = yyyy + '-' + mm + '-' + dd;
+
+    // api key
+    const api = 'JbPskfAcVPpxN602YevCVKqXG7dh7VZ7Yb8qkM2j';
+
+    // init empty array for Nasa Data
+    let [nasaData, setNasaData] = useState([]);
+    let [date, setDate] = useState(todaysDate);
+
+    // nasa api url
+    let url = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${api}`
 
 
     // const photo = <div className='w-full mx-auto flex flex-col'>
@@ -21,11 +37,36 @@ function PhotoOfTheDay(){
                         
     //                 </div>
 
+    function getData(e) {
+        e.preventDefault();
+
+        console.log("Clicked the fetch button")
+
+        axios.get(url).then((response) => console.log(response.data)).catch(err => err.message)
+
+    }
+
      
     return (
         <div className='text-white'>
 
-            <button className='text-xl bg-white my-4 outline outline-4 bg-blue-400/50 text-white outline-blue-600 w-1/3 md:w-1/4 mx-auto px-2' > Photo Of The Day </button>
+            <button className=' 
+                text-xl 
+                bg-white 
+                my-4 
+                outline 
+                outline-4 
+                bg-blue-400/50 
+                text-white 
+                outline-blue-600 
+                w-1/3 
+                md:w-1/4 
+                mx-auto 
+                px-2' 
+            
+                onClick={getData}
+
+             > Photo Of The Day </button>
             PhotoOfTheDay component 
         </div>
     )
