56bd20d8c1e3e9f273c240e37983ce699c52f549
Oscar Pacheco Ortiz <Oscarpa9188@gmail.com>
2022-04-16 22:20:17 -0500

    Updated Styles

--- nasa_pod/src/App.js
+++ nasa_pod/src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className='bg-[url("https://images.pexels.com/photos/220201/pexels-photo-220201.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")] bg-cover h-fit min-h-screen'>
-      < div className = 'container mx-auto ' >
+      < div className = 'container mx-auto flex flex-col min-h-screen justify-between' >
 
         <Navbar />
 
